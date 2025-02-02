81edb5b466779e720e56cbad10d50d2ce361415c
Daniel Kolas <daniel.piotr.kolas@gmail.com>
2025-02-02 18:12:12 +0100

    lines - deleted the old files

--- lines/sketch_03.js
+++ lines/sketch_03.js
@@ -1,66 +1,42 @@
 const canvasSketch = require('canvas-sketch');
-const random = require("canvas-sketch-util/random");
+
 
 const settings = {
-  dimensions: [ 1080, 1080 ],
-  animate: true
+  dimensions: [ 1080, 1080 ]
 };
-
-const sketch = ({ context, width, height }) => {
-  const agents = [];
-  for (let i = 0; i < 40; i++){
-    const x = random.range(0, width);
-    const y = random.range(0, height);
-    agents.push(new Agent(x, y));
-  }
-
+const sketch = () => {
   return ({ context, width, height }) => {
     context.fillStyle = 'white';
     context.fillRect(0, 0, width, height);
 
-    agents.forEach(agent => {
-      agent.update();
-      agent.draw(context);
-      agent.bounce(width, height);
-    })
+    const agentA = new Agent(800, 400);
+    const agentB = new Agent(800, 400);
+
+    agentA.draw(context);
+    agentB.draw(context);
   };
 };
 
 canvasSketch(sketch, settings);
 
-class Vector {
-  constructor(x, y) {
+
+class Point {
+  constuctor(x, y, radius) {
     this.x = x;
     this.y = y;
+    this.radius = radius;
   }
 }
+
 class Agent {
-  constructor(x, y) {
-    this.pos = new Vector(x, y);
-    this.vel = new Vector(random.range(-1, 1), random.range(-1, 1));
-    this.radius = random.range(4, 12);
+  constructor(x, y){
+    this.position = new Point(x, y);
+    this.radius = 10;
   }
 
-  bounce(width, height) {
-    if (this.pos.x <= 0 || this.pos.x >= width) this.vel.x *= -1
-    if (this.pos.y <= 0 || this.pos.y >= height) this.vel.y *= -1
-  }
-
-  update() {
-    this.pos.x += this.vel.x;
-    this.pos.y += this.vel.y;
-  }
   draw(context) {
-    context.save();
-    context.translate(this.pos.x, this.pos.y);
-
-    context.lineWidth = 4;
-
     context.beginPath();
-    context.arc(0, 0, this.radius, 0, Math.PI * 2);
+    context.arc(this.position.x, this.position.y, this.radius, 0, Math.PI * 2);
     context.fill();
-    context.stroke();
-
-    context.restore();
-  };
+  }
 }
